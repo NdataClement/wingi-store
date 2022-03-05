b9fb45bb477fad006d234b6b9dd01ac4289fea90
NdataClement <clementndatabaye@gmail.com>
2022-03-05 08:52:07 +0200

    Initial commit

---
+++ database/wingi.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 05, 2022 at 07:48 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wingi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `category`, `price`, `quantity`, `date_created`) VALUES
+(1, 'Jeans', 'Clothes', 1500, 10, '2022-03-04 23:53:03'),
+(3, 'Black diamond', 'Jewellery', 50000, 10, '2022-03-04 23:53:51'),
+(5, 'Gold necklace', 'Jewellery', 50000, 10, '2022-03-04 23:53:27'),
+(6, 'Silver watch', 'Jewellery', 10000, 8, '2022-03-04 23:56:43'),
+(9, 'Earring', 'Jewellery', 800, 20, '2022-03-04 23:57:48'),
+(10, 'Diamond ring', 'Jewellery', 50000, 10, '2022-03-04 23:54:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`) VALUES
+(1, 'example@gmail.com', '$2y$10$bg5u0kGzrfghSktfZvglWOBGCGHsPyG3LIJznkHBULZqtxAh3wpGK');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ php/editproduct.php
@@ -0,0 +1,122 @@
+<?php 
+    //Session
+    session_start();
+    $email=$_SESSION['email'];
+    if (!(isset($_SESSION['email']))) {
+        header("location:login.php");
+    }
+
+    include '../php/connect.php';
+    
+    $productname = "";
+    $category = "";
+
+    if (isset($_GET['pid'])) {
+
+        $id = $_GET['pid'];
+         $rec = mysqli_query($con,"SELECT * FROM products WHERE id='$id'");
+         
+         
+         $record = mysqli_fetch_assoc($rec);
+         $id=$record['id'];
+         $productname = $record['name'];
+         $category = $record['category'];
+         $price = $record['price'];
+         $quantity = $record['quantity'];
+
+         if(isset($_POST['update'])) 
+         {
+             $pName = $_POST['name'];
+             $categ = $_POST['category'];
+             $prices = $_POST['price'];
+             $quantities = $_POST['quantity'];
+             if($productname != $pName || $category != $categ || $price != $prices || $quantity != $quantities)
+             {
+               $update = mysqli_query($con, "UPDATE products SET name = '$pName', category = '$categ', price = '$prices', 
+               quantity = '$quantities', date_created=now() WHERE id='$id'");
+               header('location:index.php');
+             }
+             
+         }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="./../assets/WingiIcon.png" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Wingi Store</title>
+    <!-- <link rel="shortcut icon" href="./../assets/favicon_0.ico" type="image/x-icon">     -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet">
+    
+    <script src="https://code.jquery.com/jquery-4.3.1.slim.min.js"></script>
+    
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container">
+              <a class="navbar-brand" href="index.php"><img src="../assets/WingiLogo.png" height="40"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          <span><i class="icon icon-user"></i></span>&nbsp;Admin
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
+                          <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                        </ul>
+                      </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
+    <br>
+    <div class="container">
+        <div class="registration-form">
+            <form autocomplete="off" method="POST">
+                <div class="form-label">
+                    <h3>Edit Product</h3>
+                    <hr>
+                </div>
+                <div class="form-group">
+                    <label class="form-label"><b>Product name</b></label>
+                    <input type="text" class="form-control item" name="name" value="<?php echo $productname; ?>">
+                </div>
+                <div class="form-group">
+                <label class="form-label"><b>Category</b></label>
+                    <input type="text" class="form-control item" name="category" value="<?php echo $category; ?>">
+                </div>
+                <div class="form-group">
+                    <label class="form-label"><b>Price</b></label>
+                    <input type="number" class="form-control item" name="price" value="<?php echo $price; ?>">
+                </div>
+                <div class="form-group">
+                    <label class="form-label"><b>Quantity</b></label>
+                    <input type="number" class="form-control item" name="quantity" value="<?php echo $quantity; ?>">
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <input type = "submit" name = "update" value = "UPDATE" class = "btn btn-block signIn">
+                    </div>
+                    <div class="col-6">
+                        <a href = "index.php" class = "btn btn-block signIn">CLOSE</a>
+                    </div>
+                    
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
